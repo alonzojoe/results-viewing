cf1dce689d54178cb7673bc32fe1da7d2db6107f
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-24 14:39:30 +0800

    chore: setup app assets css, icons and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import "./assets/bootstrap/css/bootstrap.min.css";
+import "./assets/fontawesome/css/fontawesome.min.css";
+import "./assets/custom/main.css";
+import "./assets/css/main.css";
 function App() {
   return (
     <>
