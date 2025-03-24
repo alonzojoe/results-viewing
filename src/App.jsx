4ee83d30b54b38c62f4d8f1d658e6ec15af1c434
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-24 14:47:12 +0800

    chore(add dep): install axios, toast and router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./assets/bootstrap/css/bootstrap.min.css";
 import "./assets/fontawesome/css/fontawesome.min.css";
 import "./assets/custom/main.css";
 import "./assets/css/main.css";
+import "./assets/bootstrap/js/bootstrap.bundle.min.js";
+
 function App() {
   return (
     <>
