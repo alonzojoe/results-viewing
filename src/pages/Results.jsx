ee190752b980d0ed98072b3ba3fce916e8636232
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-25 10:32:45 +0800

    style: create ui component for Laboratory Results Transaction Number Searching

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,17 +1,31 @@
 import LogoContainer from "../components/LogoContainer";
+import LogoImg from "../assets/images/okopdlogo.png";
 
 const Results = () => {
   return (
     <div>
       <LogoContainer>
-        <img src="./src/assets/images/okopdlogo.png" alt="JBLMGH - OKOPD" />
+        <img src={LogoImg} alt="JBLMGH - OKOPD" />
       </LogoContainer>
-      <h2>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam facere
-        provident et neque consequatur hic accusamus id expedita adipisci non
-        architecto, totam harum vitae modi animi. Dignissimos quaerat ratione
-        aliquam.
-      </h2>
+      <div className="py-5 gap-2 qr-container transition-fade-in">
+        <h2 className="d-flex justify-content-center align-self-center mb-4">
+          Laboratory Results
+        </h2>
+        <>
+          <div className="w-100 mb-2">
+            <label className="fw-bold mb-1">Transaction Number</label>
+            <input
+              className="form-control"
+              placeholder="Enter Patient Transaction Number..."
+            />
+          </div>
+          <div className="btn-container mt-3 text-center">
+            <button className="btn-default btn w-100 mb-2">
+              <i className="fa fa-search" aria-hidden="true"></i> Search
+            </button>
+          </div>
+        </>
+      </div>
     </div>
   );
 };
