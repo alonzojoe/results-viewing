4b6716acb47bd8446aa90b8a835114254ce5312d
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-25 14:07:33 +0800

    style: create ui ux for List of available lab results

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -37,11 +37,7 @@ const Results = () => {
           <>
             <div className="w-100 mb-">
               <label className="fw-bold mb-1">Transaction Date</label>
-              <input
-                type="date"
-                className="form-control"
-                placeholder="Enter Patient Transaction Number..."
-              />
+              <input type="date" className="form-control" />
             </div>
             <div className="btn-container mt-3 text-center">
               <button className="btn-default btn w-100 mb-5">
@@ -50,7 +46,47 @@ const Results = () => {
             </div>
           </>
         ) : (
-          <p>Invalid Tab</p>
+          <>
+            <div className="w-100 mb-">
+              <h2 className="fs-5">List of available results</h2>
+            </div>
+            <div className="btn-container mt-3 text-center">
+              <div className="d-flex justify-content-between mb-2">
+                <div>
+                  1.{" "}
+                  <span className="cursor-pointer">
+                    <i
+                      className="fa fa-file-pdf text-danger mx-1 mr-2 fs-5"
+                      aria-hidden="true"
+                    ></i>
+                    <span className="cursor-pointer text-primary fw-bold fs-6">
+                      CBC <small>(Click to view)</small>
+                    </span>
+                  </span>
+                </div>
+                <div>
+                  <i className="fa fa-eye text-primary fs-5 cursor-pointer"></i>
+                </div>
+              </div>
+              <div className="d-flex justify-content-between mb-2">
+                <div>
+                  2.{" "}
+                  <span className="cursor-pointer">
+                    <i
+                      className="fa fa-file-pdf text-danger mx-1 mr-2 fs-5"
+                      aria-hidden="true"
+                    ></i>
+                    <span className="cursor-pointer text-primary fw-bold fs-6">
+                      Potasium K <small>(Click to view)</small>
+                    </span>
+                  </span>
+                </div>
+                <div>
+                  <i className="fa fa-eye text-primary fs-5 cursor-pointer"></i>
+                </div>
+              </div>
+            </div>
+          </>
         )}
       </div>
     </div>
