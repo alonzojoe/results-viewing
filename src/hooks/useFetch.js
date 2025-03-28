1d8f32f0a95ef3aa083dcfd18c86225edc848fb5
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-28 09:40:43 +0800

    feat: create new custom hook useFetch() use the services api.

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import api from "../services/api";
+
+const useFetch = (endpoint, params = {}) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const res = await api(endpoint, params);
+        setData(await res.data.data);
+      } catch (error) {
+        setError(error?.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [endpoint, params]);
+
+  return [data, loading, error];
+};
+
+export default useFetch;
