fe406aebc34154c7120128fbca79dc21de69f38d
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-31 09:59:50 +0800

    feat: create new Pagination component

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,12 @@
+const Pagination = () => {
+  return (
+    <div className="mt-2 d-flex justify-items-end gap-1">
+      <button className="">First</button>
+      <button className="">Previous</button>
+      <button className="">Next</button>
+      <button className="">Last</button>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/pages/Results/components/List.jsx
+++ src/pages/Results/components/List.jsx
@@ -1,12 +1,16 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useMemo, useRef, useState } from "react";
 import { PatientContext } from "../../../context/Patient/patient-context";
 import moment from "moment";
 import TitleContainer from "../../../components/TitleContainer";
 import useFetch from "./../../../hooks/useFetch";
-
+import Pagination from "../../../components/Pagination";
+const LIMIT = 10;
 const List = () => {
   const dateRef = useRef(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [paginated, setPaginated] = useState({
+    currentPage: 1,
+    display: [],
+  });
 
   const { patient: data } = useContext(PatientContext);
 
@@ -17,11 +21,25 @@ const List = () => {
   const [results, loading, error] = useFetch(`/result/${PatientHistoryID}`);
   console.log("res", results);
 
+  const display = useMemo(() => {
+    if (results) {
+      return {
+        lastpage: Math.ceil(results.length / LIMIT),
+        display: results.slice(0, LIMIT),
+      };
+    }
+    return {
+      lastpage: 0,
+      display: [],
+    };
+  }, [results]);
+
   return (
     <>
       <TitleContainer title="List of available results" />
       {JSON.stringify(loading)}
       <div className="btn-container mt-3 text-center">
+        <pre>{JSON.stringify(display)}</pre>
         {results &&
           results.map((r, index) => (
             <div className="d-flex justify-content-between mb-2" key={r.id}>
@@ -42,40 +60,7 @@ const List = () => {
               </div>
             </div>
           ))}
-        <div className="d-flex justify-content-between mb-2">
-          <div>
-            1.{" "}
-            <span className="cursor-pointer">
-              <i
-                className="fa fa-file-pdf text-danger mx-1 mr-2 fs-5"
-                aria-hidden="true"
-              ></i>
-              <span className="cursor-pointer text-primary fw-bold fs-6">
-                CBC <small>(Click to view)</small>
-              </span>
-            </span>
-          </div>
-          <div>
-            <i className="fa fa-eye text-primary fs-5 cursor-pointer"></i>
-          </div>
-        </div>
-        <div className="d-flex justify-content-between mb-2">
-          <div>
-            2.{" "}
-            <span className="cursor-pointer">
-              <i
-                className="fa fa-file-pdf text-danger mx-1 mr-2 fs-5"
-                aria-hidden="true"
-              ></i>
-              <span className="cursor-pointer text-primary fw-bold fs-6">
-                Potasium K <small>(Click to view)</small>
-              </span>
-            </span>
-          </div>
-          <div>
-            <i className="fa fa-eye text-primary fs-5 cursor-pointer"></i>
-          </div>
-        </div>
+        <Pagination />
       </div>
     </>
   );
