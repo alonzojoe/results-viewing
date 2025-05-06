ff130a30bf278e71595b579a4792601f040ff223
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-05-06 16:40:49 +0800

    feat: add the warning message of ListInfo in Instruction component

--- src/components/Instructions/Instructions.jsx
+++ src/components/Instructions/Instructions.jsx
@@ -178,7 +178,10 @@ const Instructions = ({ label, onClose }) => {
         <hr />
         <div className="mt-2 alert alert-danger" role="alert">
           <p className="mb-0">
-            <i className="fa-solid fa-triangle-exclamation"></i>
+            <i className="fa-solid fa-triangle-exclamation"></i> If your results
+            are not appearing below, it may be due to system restrictions that
+            prevent some results from being available online. In such cases,
+            please contact the JBLMGH Laboratory.
           </p>
         </div>
         <div className="mb-2">
